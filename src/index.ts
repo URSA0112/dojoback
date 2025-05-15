ad298cc5ba3cbad93c93d6ebd683574a7100ec92
URSA0112 <Nimone99@gmail.com>
2025-05-15 17:28:56 +0800

    Generated folder deleted , index.ts changes 2 small change

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,18 @@ app.use(express.json());
 
 app.use("/api/v1/teacher", teacherRoutes);
 
-app.use((req: Request, res: Response) => {
-  res.status(404).json({ error: "Route not found" });
+//Энэ бол сервер ажиллаж байгаа эсэхийг шалгах тест 
+app.get("/", (req: Request, res: Response) => {
+  try{
+  res.send("✅ Server running...")}
+  catch (error) {
+    console.error("Error in root route:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 const PORT = process.env.PORT || 8000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server listening at: http://localhost:${PORT}`);
 });
